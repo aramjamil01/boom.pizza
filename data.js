3a1b521a1e309f9b8e55725d4b8af8903ea41d66
aramkurdish <arambarsa@gmail.com>
2026-01-01 02:33:18 +0300

    Create data.js

---
+++ data.js
@@ -0,0 +1,9 @@
+const menuItems = [
+    { id: 1, name: "پیزای گۆشت", price: 5000, category: "pizza", img: "https://...", ingredients: "گۆشت، پەنیر" },
+    // هەموو خواردنەکانی تر لێرە دابنێ...
+];
+
+const toppingsList = [
+    { id: 'cheese', name: 'پەنیری زیادە', price: 1000 },
+    // زیادکراوەکانی تر لێرە دابنێ...
+];
