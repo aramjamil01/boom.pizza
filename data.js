51b8342996260b05d8ad8741dcfdb20d14cfe385
aramkurdish <arambarsa@gmail.com>
2026-01-01 02:44:23 +0300

    Delete data.js

--- data.js
+++
@@ -1,9 +0,0 @@
-const menuItems = [
-    { id: 1, name: "پیزای گۆشت", price: 5000, category: "pizza", img: "https://...", ingredients: "گۆشت، پەنیر" },
-    // هەموو خواردنەکانی تر لێرە دابنێ...
-];
-
-const toppingsList = [
-    { id: 'cheese', name: 'پەنیری زیادە', price: 1000 },
-    // زیادکراوەکانی تر لێرە دابنێ...
-];
